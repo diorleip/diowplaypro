669dda489bdce72c613dbaf540a6f3931734f72a
Diorlei Pinheiro <pdiorlei@gmail.com>
2026-06-07 11:27:19 -0300

    Atualiza metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata:
 Metadata = {
 
   title:
-    "DiowPlay Pro",
+    "gerador.diowplay",
 
   description:
     "Plataforma Premium"
---
+++ data/Abrir-DiowPlay.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\projetos diowplay\diowplaypro"
+powershell
